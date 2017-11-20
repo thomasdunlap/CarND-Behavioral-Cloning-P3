ccd55bfef5a355f1ac7479e5cf67ce3cb53c9e7f
thomasdunlap <tom.a.dunlap@gmail.com>
2017-11-20 10:32:33 -0500

    Added Nvidia-esque model.

--- model.py
+++ model.py
@@ -38,6 +38,26 @@ def load_data(args):
 
     return X_train, X_test, y_train, y_test
 
+
+model = Sequential()
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(64, 5, 5, activation='relu'))
+model.add(Convolution2D(64, 5, 5, activation='relu'))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+
+model.summary()
+
+
+
+
 def build_model(args):
     pass
 
