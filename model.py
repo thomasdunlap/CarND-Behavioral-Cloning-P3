f04ebbbebdc3e10377ca08054d5e723a50cf5ba2
thomasdunlap <tom.a.dunlap@gmail.com>
2017-11-21 17:50:26 -0500

    Creates crappy model that works [exclamation].

--- model.py
+++ model.py
@@ -15,6 +15,61 @@ from keras.layers.convolutional import Convolution2D
 import argparse # Reading command line arguments
 import os # Reading files
 
+import os
+import csv
+
+samples = []
+with open('./driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        samples.append(line)
+
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+
+import cv2
+import numpy as np
+import sklearn
+
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                name = './IMG/'+batch_sample[0].split('/')[-1]
+                center_image = cv2.imread(name)
+                center_angle = float(batch_sample[3])
+                images.append(center_image)
+                angles.append(center_angle)
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
+
+ch, row, col = 3, 80, 320  # Trimmed image format
+
+model = Sequential()
+# Preprocess incoming data, centered around zero with small standard deviation
+model.add(Lambda(lambda x: x/127.5 - 1.,
+        input_shape=(ch, row, col),
+        output_shape=(ch, row, col)))
+model.add(... finish defining the rest of your model architecture here ...)
+
+model.compile(loss='mse', optimizer='adam')
+model.fit_generator(train_generator, samples_per_epoch= /
+            len(train_samples), validation_data=validation_generator, /
+            nb_val_samples=len(validation_samples), nb_epoch=3)
+
 path = '/Users/tomdunlap/projects/udacity/sd_car/CarND-Behavioral-Cloning-P3/data/'
 angle_adjustment = 0.1
 
@@ -184,6 +239,6 @@ model.add(Dense(1))
 model.summary()
 
 model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
-model.fit(X_train, y_train, validation_data=(X_test, y_test), nb_epoch=3, shuffle=True)
+model.fit(X_train, y_train, validation_data=(X_test, y_test), nb_epoch=10, shuffle=True)
 
 model.save('model_mine.h5')
