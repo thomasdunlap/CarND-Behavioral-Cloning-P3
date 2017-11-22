d6c430b507c27f48c2ea6905b2712d3ffb6afe80
thomasdunlap <tom.a.dunlap@gmail.com>
2017-11-21 22:47:35 -0500

    Fit generator model and some writeup.

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import pandas as pd # Read and update data
 import csv # Read csv files
 import cv2 # OpenCV2 for image processing
 # Splits data into training and test sets
+import sklearn
 from sklearn.model_selection import train_test_split
 # Shuffle order of training set each epoch
 from sklearn.utils import shuffle
@@ -15,21 +16,14 @@ from keras.layers.convolutional import Convolution2D
 import argparse # Reading command line arguments
 import os # Reading files
 
-import os
-import csv
 
 samples = []
-with open('./driving_log.csv') as csvfile:
+with open('./data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         samples.append(line)
 
-from sklearn.model_selection import train_test_split
-train_samples, validation_samples = train_test_split(samples, test_size=0.2)
-
-import cv2
-import numpy as np
-import sklearn
+train_samples, validation_samples = train_test_split(samples[1:], test_size=0.2)
 
 def generator(samples, batch_size=32):
     num_samples = len(samples)
@@ -41,11 +35,15 @@ def generator(samples, batch_size=32):
             images = []
             angles = []
             for batch_sample in batch_samples:
-                name = './IMG/'+batch_sample[0].split('/')[-1]
+                name = './data/IMG/'+batch_sample[0].split('/')[-1]
                 center_image = cv2.imread(name)
+                center_image_rgb = cv2.cvtColor(center_image, cv2.COLOR_BGR2RGB)
                 center_angle = float(batch_sample[3])
-                images.append(center_image)
+                images.append(center_image_rgb)
                 angles.append(center_angle)
+                # Flipped images
+                images.append(cv2.flip(center_image_rgb, 1))
+                angles.append(-float(line[3]))
 
             # trim image to only see section with road
             X_train = np.array(images)
@@ -56,24 +54,34 @@ def generator(samples, batch_size=32):
 train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
-ch, row, col = 3, 80, 320  # Trimmed image format
+ch, row, col = 160, 320, 3  # Trimmed image format
 
 model = Sequential()
 # Preprocess incoming data, centered around zero with small standard deviation
-model.add(Lambda(lambda x: x/127.5 - 1.,
-        input_shape=(ch, row, col),
-        output_shape=(ch, row, col)))
-model.add(... finish defining the rest of your model architecture here ...)
+model.add(Lambda(lambda x: x / 127.5 - 1.,input_shape=(ch, row, col),output_shape=(ch, row, col)))
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+
+model.summary()
 
 model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator, samples_per_epoch= /
-            len(train_samples), validation_data=validation_generator, /
-            nb_val_samples=len(validation_samples), nb_epoch=3)
+model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3)
 
+model.save('model_mine.h5')
+'''
 path = '/Users/tomdunlap/projects/udacity/sd_car/CarND-Behavioral-Cloning-P3/data/'
 angle_adjustment = 0.1
 
-'''
+
 #for debugging, allows for reproducible/deterministic results
 np.random.seed(0)
 
@@ -92,7 +100,7 @@ def load_data(args):
     X_train, X_test, y_train, y_test = train_test_split(X, y test_size=args.test_size, random_state=0)
 
     return X_train, X_test, y_train, y_test
-'''
+
 
 def img_process(X, y, line):
     """
@@ -117,11 +125,10 @@ def img_process(X, y, line):
 
     return X, y
 
-'''
+
 for line in reader:
     for i in range(3):
         images, angles = img_process(images, angles, line, i)
-'''
 
 lines = []
 images = []
@@ -141,7 +148,7 @@ with open('./data/driving_log.csv') as csvfile:
         #flipped
         images.append(cv2.flip(center_image_rgb, 1))
         angles.append(-float(line[3]))
-'''
+
         left_image = cv2.imread('./data/IMG/' + line[1].split('/')[-1])
         left_image_rgb = cv2.cvtColor(left_image, cv2.COLOR_BGR2RGB)
 
@@ -159,10 +166,6 @@ with open('./data/driving_log.csv') as csvfile:
         #flipped
         images.append(cv2.flip(right_image_rgb, 1))
         angles.append(-(float(line[3])-angle_adjustment))
-'''
-
-
-'''
 
 
 
@@ -191,10 +194,7 @@ for line in lines[1:]:
         angles.append(-float(line[3]))
 
 
-'''
 
-
-'''
     source_path = line[0]
     filename = source_path.split('/')[-1]
     current_path = './data/IMG/' + filename
@@ -203,24 +203,21 @@ for line in lines[1:]:
     images.append(image)
     angle = float(line[3])
     angles.append(angle)
-'''
-'''
-        print
 
-'''
+
 images = np.array(images)
 angles = np.array(angles)
 
 # Split data to training/test : 80%/20%
 X_train, X_test, y_train, y_test = train_test_split(images, angles, test_size=0.2)
 
-'''
+
 # Convert to numpy for Keras
 X_train = np.array(X_train)
 y_train = np.array(y_train)
 X_test = np.array(X_test)
 y_test = np.array(y_test)
-'''
+
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
@@ -242,3 +239,4 @@ model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
 model.fit(X_train, y_train, validation_data=(X_test, y_test), nb_epoch=10, shuffle=True)
 
 model.save('model_mine.h5')
+'''
